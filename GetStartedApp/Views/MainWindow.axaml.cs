c64dab6b705370b27ea244475793abea2083e280
garrett-low <usernamegl@live.com>
2024-08-29 23:01:43 -0500

    Finish Avalon tutorial

--- GetStartedApp/Views/MainWindow.axaml.cs
+++ GetStartedApp/Views/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Diagnostics;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 
 namespace GetStartedApp.Views;
 
@@ -7,5 +11,36 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void ButtonClicked(object source, RoutedEventArgs args)
+    {
+        Debug.WriteLine("Click!");
+        Debug.WriteLine($"Click! Celsius={celsius.Text}");
+
+        if (double.TryParse(celsius.Text, out double celsiusOut))
+        {
+            double fahrenheitCalc = (celsiusOut * (9d / 5d)) + 32;
+            fahrenheit.Text = fahrenheitCalc.ToString("0.0");
+        }
+        else {
+            celsius.Text = "0";
+            fahrenheit.Text = "0";
+        }
+    }
+
+    public void OnTextInputCelsius(object source, TextChangedEventArgs args)
+    {
+        Debug.WriteLine($"Type: {celsius.Text}");
+        if (double.TryParse(celsius.Text, out double celsiusOut))
+        {
+            double fahrenheitCalc = (celsiusOut * (9d / 5d)) + 32;
+            fahrenheit.Text = fahrenheitCalc.ToString("0.0");
+        }
+        else
+        {
+            celsius.Text = "0";
+            fahrenheit.Text = "0";
+        }
     }
 }
